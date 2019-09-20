351d723acf833eb8d8c8fd01f5afdf67cfe04fea
oa8oht <leahliu779@gmail.com>
2019-09-19 20:17:32 -0700

    Updates

---
+++ precache-manifest.48ae9274d7962e515f24f4ce59c831ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de220ee0e4b00782fe822c9b88fae738",
+    "url": "/bugsweeper/index.html"
+  },
+  {
+    "revision": "d52c0e501b5161765beb",
+    "url": "/bugsweeper/static/css/main.a7bfaa43.chunk.css"
+  },
+  {
+    "revision": "d41824e2e25ca3775d6b",
+    "url": "/bugsweeper/static/js/2.e1c1f8c2.chunk.js"
+  },
+  {
+    "revision": "d52c0e501b5161765beb",
+    "url": "/bugsweeper/static/js/main.75ad089e.chunk.js"
+  },
+  {
+    "revision": "e3afa592894b0ee453fe",
+    "url": "/bugsweeper/static/js/runtime~main.86b0ab02.js"
+  },
+  {
+    "revision": "13d0456925720dc1c056b0a3b32467c7",
+    "url": "/bugsweeper/static/media/bug.13d04569.svg"
+  },
+  {
+    "revision": "8ab36a6d544197ae21280934a02821c9",
+    "url": "/bugsweeper/static/media/face.8ab36a6d.svg"
+  }
+]);
